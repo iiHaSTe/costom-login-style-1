1ca30b7fe1029d3e37c7475a6ceb03d27d18f196
www.hicham2005@gmail.com <www.hicham2005@gmail.com>
2022-05-22 16:39:34 +0100

    jdjdf

--- main.js
+++ main.js
@@ -2,21 +2,40 @@
 
 const email = document.getElementById("email");
 const pass = document.getElementById("password");
+const emailIcon = document.getElementById("email-icon");
+const passIcon = document.getElementById("pass-icon");
 
-const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+
+const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ig
 email.addEventListener('input', ()=>{
-  if (email.value.match(emailPattern))
+  if (email.value.match(emailPattern)){
     email.style.borderColor = "rgb(27, 194, 48)";
-  else if (email.value.length === 0)
+    emailIcon.style.color = "rgb(27, 194, 48)";
+  }else if (email.value.length === 0){
     email.style.borderColor = "rgb(213, 213, 213)";
-  else
+    emailIcon.style.color = "grey";
+  }else{
     email.style.borderColor = "rgb(194, 70, 27)";
+    emailIcon.style.color = "rgb(194, 70, 27)";
+  }
 });
 pass.addEventListener('input', ()=>{
-  if (pass.value.length >= 8)
+  if (pass.value.length >= 8){
     pass.style.borderColor = "rgb(27, 194, 48)";
-  else if (pass.value.length === 0)
+    passIcon.style.color = "rgb(27, 194, 48)";
+  }else if (pass.value.length === 0){
     pass.style.borderColor = "rgb(213, 213, 213)";
-  else
+    passIcon.style.color = "grey";
+  }else{
     pass.style.borderColor = "rgb(194, 70, 27)";
+    passIcon.style.color = "rgb(194, 70, 27)";
+  }
+});
+
+passIcon.addEventListener("click", ()=>{
+  if (passIcon.classList.toggle("fa-lock-open"))
+    pass.setAttribute("type", "text");
+  else
+    pass.setAttribute("type", "password");
 });
